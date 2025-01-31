7a3acc792173d853a15f8af197d24ec25ad52944
AmirUCR <amohs002@ucr.edu>
2025-01-30 19:51:37 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extensions = [
 
 setup(
     name='mstmap',
-    version='1.1.3',
+    version='1.1.4',
     author='Amirsadra Mohseni',
     author_email='amohs002@ucr.edu',
     description='A Python library for genetic mapping',
